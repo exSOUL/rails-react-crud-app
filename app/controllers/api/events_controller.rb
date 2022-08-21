4fa0e9ffa831973f8ddfce3702fbe347f19717d0
ex_SOUL <soul@soul-macbook-air.local>
2022-08-21 23:22:15 +0900

    rubocop -a

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,2 +1,43 @@
 class Api::EventsController < ApplicationController
+  before_action :set_event, only: %i[show update destroy]
+
+  def index
+    @event = Event.all
+    render json: @event
+  end
+
+  def show
+    render json: @event
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render json: @event, status: :created
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      render json: @event, status: :ok
+    else
+      render json: @event.errors, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # def event_params
+  # end
 end

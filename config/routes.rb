eeea90be498f322803e78ae0848d28a6e6eaef14
ex_SOUL <ex.soul.1984.cosmos@gmail.com>
2022-08-27 16:09:23 +0900

    うごかねーと思ってたらresoucesをresourceってTypoしてた件について

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   root to: 'site#index'
 
   namespace :api do
-    resource :events, only: %i[index show create destroy update]
+    resources :events, only: %i[index show create destroy update]
   end
 end
